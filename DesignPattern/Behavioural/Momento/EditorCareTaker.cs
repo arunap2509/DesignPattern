591474a1ac6f55ec220b6d79ce32fcfbccffdee5
Arun AP <arunap@192.168.0.103>
2022-07-16 18:06:18 +0530

    refactoring

---
+++ DesignPattern/Behavioural/Momento/EditorCareTaker.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPattern.Behavioural.Momento;
+
+public class EditorCareTaker
+{
+    private readonly Stack<EditorMomento> _editorMomentos;
+    private readonly Editor _editor;
+
+    public EditorCareTaker(Editor editor)
+    {
+        _editorMomentos = new();
+        _editor = editor;
+    }
+
+    public void Backup()
+    {
+        _editorMomentos.Push(_editor.SaveMomento());
+    }
+
+    public void Undo()
+    {
+        var momento = _editorMomentos.Pop();
+
+        _editor.Restore(momento);
+    }
+
+    public void ListBackup()
+    {
+        foreach (var momento in _editorMomentos)
+        {
+            Console.WriteLine($"backup with content as {momento.editorContent} " +
+                $"and (x,y) in ({momento.cursorRow}, {momento.cursorColumn})");
+        }
+    }
+}
---
+++ DesignPattern/Behavioural/Momento/EditorMomento.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPattern.Behavioural.Momento;
+
+public class EditorMomento
+{
+    public readonly int cursorRow;
+    public readonly int cursorColumn;
+    public readonly string editorContent;
+
+    public EditorMomento(int x, int y, string content)
+    {
+        cursorRow = x;
+        cursorColumn = y;
+        editorContent = content;
+    }
+}
